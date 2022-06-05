dae6f4eb7ec2e816d1ea02b624556e992943aa17
zhejiushibao <1966220492@qq.com>
2022-06-05 16:41:36 +0800

    Site updated: 2022-06-05 16:41:35

--- js/load-aplayer.js
+++ js/load-aplayer.js
@@ -12,6 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
           url: 'https://cdn.jsdelivr.net/gh/zhejiushibao/yinyue@main/1.mp3',
           cover: 'http://p1.music.126.net/3x4HLyzg9K5OXbTWttbRYA==/109951165073688416.jpg?param=130y130',
         },
+        {
+          name: '倾尽天下',
+          artist: '河图',
+          url: 'https://cdn.jsdelivr.net/gh/zhejiushibao/yinyue@main/%E6%B2%B3%E5%9B%BE%20-%20%E5%80%BE%E5%B0%BD%E5%A4%A9%E4%B8%8B.mp3',
+          cover: 'http://p1.music.126.net/nlocSgc4D7T-h6S3f6IybQ==/6003333487842277.jpg?param=130y130'
+        }
       ],
     })
   })
